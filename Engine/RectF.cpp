4d7c258e6bdd78e4f409eef81e96294fc9b42713
jsar26606@gmail.com <jsar26606@whatever.com>
2023-05-31 04:38:57 -0700

    the ball's bouncing off of the bottom and right screen !

--- Engine/RectF.cpp
+++ Engine/RectF.cpp
@@ -11,13 +11,15 @@ RectF::RectF(int left_in, int right_in, int top_in, int bottom_in)
 }
 
 RectF::RectF(const Vec2& topLeft, const Vec2& bottomRight)
+	:
+	RectF(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y)
 {
-	RectF(topLeft.x, bottomRight.x, topLeft.y, bottomRight.y);
 }
 
 RectF::RectF(const Vec2& topLeft, float width, float height)
+	:
+	RectF(topLeft, Vec2(width, height))
 {
-	RectF(topLeft, Vec2(width, height));
 }
 
 RectF RectF::FromCentre(const Vec2& centre, float halfWidth, float halfHeight)
